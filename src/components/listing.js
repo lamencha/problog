d6bc44fb3bde103ddab7ef005aa8e6c0c3d33dfa
Luis Menchaca <Luis Menchaca>
2019-02-03 19:57:17 -0700

    Post layout changed, read more button changed

--- src/components/listing.js
+++ src/components/listing.js
@@ -30,33 +30,30 @@ const LISTING_QUERY = graphql `
 const Post = styled.article`
 background-image: linear-gradient(141deg, #9fb8ad 0%, #1fc8db 51%, #2cb5e8 75%);
     box-shadow: 0px 2px 10px rgba(25, 17, 34, 0.4);
-    padding: 1.5rem;
-    border-radius: 4px;
-    margin: 15px;
-    color: #f4f4f4;
-    justify-content: space-evenly;
-     
-
     
+    border-radius: 4px;
+    margin: 15px; 
+    color: #f4f4f4;
+         
     a {
         color: #f4f4f4;
         text-decoration: none;
+        text-shadow: 2px 2px 4px #7f7f7f;
+        margin-top: 0px;
         
     }
     a:hover  {
         text-shadow: 2px 2px 4px #7f7f7f;
+        margin-top: 0px;
+        color: #c1edff;
 
     }
-    h1 {
-        margin-bottom: 0;
-    }
+
     p {
         font-size: 1.4rem;
-        margin-bottom: 0;
         display: block;
     }
 
-    
     .read-more {
         background-color: #70BFE0 ;
         box-shadow: 0px 3px 10px rgba(25, 17, 34, 0.1);
@@ -65,41 +62,54 @@ background-image: linear-gradient(141deg, #9fb8ad 0%, #1fc8db 51%, #2cb5e8 75%);
         padding: 15px 32px;
         text-align: center;
         text-decoration: none;
-        display: inline-block;
-        
-        margin: 4px 2px;
+        display: block;
+             
         cursor: pointer;
-        font-size: 0.8rem;
+        font-size: 1rem;
         text-decoration: none;
         color: #f4f4f4;
-        transition: font-size 1s;
+        transition: font-size 1.2s;
         transition: padding 1s;
-        margin-top: 15px;
+       
     }
     .read-more:hover {
+        background-color: #5ea4c1 ;
         color: #f4f4f4;
-        font-size: 0.8rem;
-        padding: 15px 50px;
+        font-size: 1rem;
+        
     }
     .dot {
-        height: 150px;
-        width: 150px;
+        height: 250px;
+        width: 250px;
         display: inline-block;
-        
-
         
       }
 
       .rate {
         height: 10%;
         width: 10%;
-        
         display: block;
         text-align: left;
-
-        
+ 
       }
 
+      .side {
+        display: inline-block;
+        width: 70%;
+        height : 100%;
+        margin-top: 5px;
+        
+        
+
+    }
+      .side1 {
+        display: inline-block;
+        padding: 1.5rem;
+             
+      }
+      h1 {
+          margin-top: 0px;
+      }
 
 
 `
@@ -111,7 +121,8 @@ const Listing = () => (
             allMarkdownRemark.edges.map(({node}) => (
                 
                 <Post key={node.frontmatter.slug}>
-                
+                <div class="side1"><img class="dot" src={node.frontmatter.image} alt="article tumbnail" /></div>
+                <div class="side">
                     <Link to={`/posts${node.frontmatter.slug}`}>
                         <h1>{node.frontmatter.title}</h1>
                         
@@ -119,12 +130,13 @@ const Listing = () => (
                     <p>{node.frontmatter.date} </p>
                     <img class="rate" src={node.frontmatter.rate} alt="Rating" />
                                                             
-                    <img class="dot" src={node.frontmatter.image} alt="article tumbnail" />
+                    
                                                            
                     <p>{node.excerpt} </p>
+                    
+                    <br></br>
+                    </div> 
                     <Link class="read-more" to={`/posts${node.frontmatter.slug}`}>Read More</Link>
-                    
-                    
                 </Post>
                 
             ))
